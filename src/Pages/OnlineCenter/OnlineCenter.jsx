c3ce2491f3bb0b4d365e2edf30feb7e66311c84b
Jahed Ahmed Ripon <73092252+jahedripon9@users.noreply.github.com>
2023-12-13 19:55:39 +0600

    updating

--- src/Pages/OnlineCenter/OnlineCenter.jsx
+++ src/Pages/OnlineCenter/OnlineCenter.jsx
@@ -4,7 +4,7 @@ const OnlineCenter = () => {
         <div className='my-10'>
             <div className="hero min-w-screen bg-white">
                 <div className="hero-content flex-col lg:flex-row ">
-                    <img src={img} className="max-w-sm rounded-lg shadow-2xl" />
+                    <img src={img} className="max-h-sm rounded-lg shadow-2xl" />
                     <div className='sm:mx-20'>
                         <h1 className="text-5xl font-bold">Welcome to the <br /> online centers</h1>
                         <p className="py-6">* Start learning from your experience <br /> * Enhance your skills with us now <br />* Do your favorite course</p>
--- src/Pages/StartLearning/StartLearning.jsx
+++ src/Pages/StartLearning/StartLearning.jsx
@@ -4,7 +4,7 @@ const Banner = () => {
         <>
             <div className="hero min-w-screen bg-white">
                 <div className="hero-content flex-col lg:flex-row-reverse sm:my-8">
-                    <img src={img} className="max-w-sm rounded-lg shadow-2xl" />
+                    <img src={img} className="max-h-sm rounded-lg shadow-2xl" />
                     <div className='mx-auto sm:px-10'>
                         <h1 className="text-4xl font-bold ">Start learning by <br /> creating free account <br /> and get register</h1>
                         
